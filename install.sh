12c3da24367a58e1d869aad694320b4737f1050f
SARTAMP <139582244+SARTAMP@users.noreply.github.com>
2024-02-16 09:56:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,20 +7,11 @@ yogz=domain
 bot=/etc/yogz
 apt update && apt upgrade -y
 apt install python3 python3-pip -y
-cd $bot
 git clone https://github.com/SARTAMP/${yogz}.git
 cd domain
 pip3 install -r requirements.txt
+python3 domain.py
 clear
-echo -e "==================================="
-echo -e "  Simple Bot Telegram create SSH"
-echo -e "==================================="
-echo -e " Masukan Info Bot Kamu ! "
-read -e -p "[*] Masukan Token Bot kamu : " token
-read -e -p "[*] Masukan Id Telegram Kamu : " admin
-echo -e ADMIN='"'$admin'"' >> $bot/$yogz/var.txt
-echo -e BOT_TOKEN='"'$token'"' >> $bot/$yogz/var.txt
-cd
 cat >/etc/systemd/system/domain.service << EOF
 [Unit]
 Description=domain
